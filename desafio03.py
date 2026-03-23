95b9f7fead2e6e7841bacc52c9618dcaf92ee9e2
laurapicari <laurapicari12@gmail.com>
2026-03-23 13:57:08 -0300

    desafio01.py

---
+++ desafio03.py
@@ -0,0 +1,8 @@
+print("Vamos montar uma soma?")
+primeiro_numero = int(input("Digite o primeiro número: "))
+segundo_numero = int(input("Digite o segundo número: "))
+
+resultado = int(primeiro_numero+segundo_numero)
+print(f"Restudado: {resultado}")
+
+
